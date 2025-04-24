58fc23f3cad81730effcc48204b7c25baff526d5
Sengtri Bun <sengtri457@gmail.com>
2025-04-24 23:21:10 +0700

    add files

--- index.js
+++ index.js
@@ -648,29 +648,6 @@ clickFav.forEach((fav) => {
     heart.classList.toggle("fa-solid");
   });
 });
-
-console.dir(clickFav);
-const buttons = document.querySelectorAll("[data-carousel-button]");
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-    const carousel = button.closest("[data-carousel]");
-    if (!carousel) return;
-
-    const slides = carousel.querySelector("[data-slides]");
-    if (!slides) return;
-
-    const activeSlide = slides.querySelector("[data-active]");
-    if (!activeSlide) return;
-
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-    if (newIndex < 0) newIndex = slides.children.length - 1;
-    if (newIndex >= slides.children.length) newIndex = 0;
-
-    slides.children[newIndex].dataset.active = true;
-    delete activeSlide.dataset.active;
-  });
-});
 function setupGallery({ galleryId, cardClass, backBtnClass, nextBtnClass }) {
   let scrollIndex = 0;
   const gallery = document.getElementById(galleryId);
